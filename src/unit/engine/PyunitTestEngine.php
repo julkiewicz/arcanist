17f4c0e1cb3fbe974862f82ef223fd5eefe41f1a
julkiewicz <julek.kopczewski@gmail.com>
2013-10-27 21:04:43 -0700

    Adding Pyunit test runner.

---
+++ src/unit/engine/PyunitTestEngine.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Very basic 'unittest.py' unit test engine wrapper.
+ *
+ * @group unitrun
+ */
+final class PyunitTestEngine extends ArcanistBaseUnitTestEngine {
+
+  public function run() {
+    $pyunit_tmp = new TempDirectory(TRUE);
+    $cmd_line = csprintf('python runtests.py --xml-output=%s discover', $pyunit_tmp);
+    $future = new ExecFuture('%C', $cmd_line);
+    list($stdout, $stderr) = $future->resolve();
+    $files = $this->getFiles((string)$pyunit_tmp);
+    return $this->getResults($files);
+  }
+
+  private function getResults($files) {
+    $parser = new ArcanistXUnitTestResultParser();
+    $results = array();
+    foreach ($files as $file) {
+      $contents = Filesystem::readFile($file);
+      $res = $parser->parseTestResults($contents);
+      $results = array_merge($results, $res);
+    }
+    return $results;
+  }
+
+  private function getFiles($dir) {
+    $entries = scandir($dir);
+    $files = array();
+    foreach ($entries as $entry) {
+      if (is_file($dir.'/'.$entry)) {
+        $files[] = $dir.'/'.$entry;
+      }
+    }
+    return $files;
+  }
+
+}
